3c52f1b1a08c62fb908b325192c16f5db8492576
guleixin <18017158729@163.com>
2025-03-27 17:26:08 +0800

    ✨ feat: preload

---
+++ preload.js
@@ -0,0 +1,7 @@
+const {contextBridge, ipcRenderer}  = require('electron')
+contextBridge.exposeInMainWorld('versions', {
+    node: () => process.version.node,
+    chrome:()=> process.versions.chrome,
+    electron:()=>process.versions.electron,
+    ping: ()=>ipcRenderer.invoke('ping')
+})
